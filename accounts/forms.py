9519612c3d1ddaf062036336a7f970258338a540
Karpovich <pmark2828@gmail.com>
2025-10-31 22:04:48 +0200

    Fix login form method and ensure it works properly

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,6 +18,8 @@ class LoginForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.form_action = ''
         self.helper.layout = Layout(
             'username',
             'password',
